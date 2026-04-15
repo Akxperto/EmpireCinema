2b6e6a128bee7db656a36096c8642b314317ff89
Akxperto7 <akxperto7@gmail.com>
2026-04-15 16:53:44 +0100

    movie controller and service

--- src/main/java/uk/ac/richmond/EmpireCinema/controller/MovieController.java
+++ src/main/java/uk/ac/richmond/EmpireCinema/controller/MovieController.java
@@ -1,13 +1,25 @@
 package uk.ac.richmond.EmpireCinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.ac.richmond.EmpireCinema.entity.Seat;
+import uk.ac.richmond.EmpireCinema.service.MovieService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController
 {
     @Autowired
-    private SeatController seatController;
+    private MovieService movieService;
+
+    @GetMapping
+
+    public List<Seat> getSeats()
+    {
+        return movieService.get
+    }
 }
--- src/main/java/uk/ac/richmond/EmpireCinema/service/MovieService.java
+++ src/main/java/uk/ac/richmond/EmpireCinema/service/MovieService.java
@@ -1,4 +1,34 @@
 package uk.ac.richmond.EmpireCinema.service;
 
-public class MovieService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import uk.ac.richmond.EmpireCinema.entity.Movie;
+import uk.ac.richmond.EmpireCinema.repository.MovieRepo;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService implements UserDetailsService
+{
+    @Autowired
+    private MovieRepo movieRepo;
+
+    public List<Movie> getAllMovies()
+    {
+        return movieRepo.findAll();
+    }
+    public Optional<Movie> getMovieById(int id)
+    {
+        return movieRepo.findById(id);
+    }
+    public Movie addMovie(Movie movie)
+    {
+        return movieRepo.save(movie);
+    }
+    public List<Movie> getMovieByTitle(String title)
+    {
+        return movieRepo.findByTitle(title);
+    }
 }
